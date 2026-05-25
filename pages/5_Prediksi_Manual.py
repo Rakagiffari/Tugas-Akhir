e42163196788fe1863c2b055648f235974958a4d
Rakagiffari <rakagiffari945@gmail.com>
2026-05-25 14:21:30 +0700

    Update 5_Prediksi_Manual.py

--- pages/5_Prediksi_Manual.py
+++ pages/5_Prediksi_Manual.py
@@ -4,19 +4,36 @@ import joblib
 
 st.title("🔍 Prediksi Manual")
 
+# LOAD
 model = joblib.load("model/random_forest_model.pkl")
+label_encoders = joblib.load("model/label_encoders.pkl")
 feature_columns = joblib.load("model/feature_columns.pkl")
 
 input_data = {}
 
 for col in feature_columns:
 
-    value = st.number_input(
-        f"Input {col}",
-        value=0.0
-    )
+    if col in label_encoders:
 
-    input_data[col] = value
+        options = list(label_encoders[col].classes_)
+
+        selected = st.selectbox(
+            f"Pilih {col}",
+            options
+        )
+
+        encoded = label_encoders[col].transform([selected])[0]
+
+        input_data[col] = encoded
+
+    else:
+
+        value = st.number_input(
+            f"Input {col}",
+            value=0.0
+        )
+
+        input_data[col] = value
 
 if st.button("Prediksi"):
 
@@ -24,4 +41,12 @@ if st.button("Prediksi"):
 
     prediction = model.predict(input_df)[0]
 
-    st.success(f"Hasil Prediksi Service : {prediction}")
+    probabilities = model.predict_proba(input_df)
+
+    confidence = probabilities.max() * 100
+
+    st.success(f"""
+    Prediksi Service : {prediction}
+
+    Confidence : {confidence:.2f}%
+    """)
